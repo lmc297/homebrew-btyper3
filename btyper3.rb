f480e96f1d88454736bf778c8ef0ca499fa3fe6c
Laura Carroll <lmc297@cornell.edu>
2020-06-28 16:13:54 +0200

    Update btyper3.rb

--- btyper3.rb
+++ btyper3.rb
@@ -1,9 +1,9 @@
 class Btyper3 < Formula
   desc "A command line tool for standardized taxonomic classification of Bacillus cereus group genome assemblies"
   homepage "https://github.com/lmc297/BTyper3/"
-  url "https://github.com/lmc297/BTyper3/raw/master/archive/btyper-3.0.2.tar.gz"
+  url "https://github.com/lmc297/BTyper3/raw/master/archive/btyper-3.1.0.tar.gz"
   sha256 "d144e9d59890473c8e1fac5614212f9e5d63d8bc7d123ce0705592110f21937c"
-  version "3.0.2"
+  version "3.1.0"
   
   depends_on "blast"
   #depends_on "fastani"
@@ -11,7 +11,7 @@ class Btyper3 < Formula
   bottle :unneeded 
    
   def install
-    bin.install "ani.py", "blast.py", "btyper3", "build_btyper3_ani_db.py", "print_final_results.py", "seq_ani_db", "seq_bt_db", "seq_virulence_db"
+    bin.install "ani.py", "blast.py", "btyper3", "build_btyper3_ani_db.py", "mlst.py", "print_final_results.py", "seq_ani_db", "seq_bt_db", "seq_mlst_db", "seq_panC_db", "seq_virulence_db"
     puts "done with env"
   end
 end
